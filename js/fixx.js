23da7c8dcfbf328fb0c6fd96c823cabbafa85394
Jython-master <43645058+Jython-master@users.noreply.github.com>
2021-04-02 13:05:08 -0700

    Charts ok

--- js/fixx.js
+++ js/fixx.js
@@ -10,30 +10,40 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		{
 			type: "scatter",
 			dataPoints: [
-				{ x: 0.204898, y: 0.461359, label: "01/08/2021", color: "#4e73df" },
-				{ x: 0.165018, y:  0.441554, label: "01/15/2021", color: "#4e73df" },
-				{x: 0.178367, y: 0.460202, label: "01/22/2021", color: "#4e73df" },
-				{ x: 0.143397, y:  0.489541, label: "01/29/2021", color: "#4e73df" },
-                { x: 0.144227, y:  0.475953, label: "02/05/2021", color: "#4e73df" }
+				{ x: 0.204898, y: 0.461359, label: "01/08/2021", color: "#4e73df", markerSize: 10 },
+				{ x: 0.165018, y:  0.441554, label: "01/15/2021", color: "#4e73df", markerSize: 10 },
+				{x: 0.178367, y: 0.460202, label: "01/22/2021", color: "#4e73df", markerSize: 10 },
+				{ x: 0.143397, y:  0.489541, label: "01/29/2021", color: "#4e73df", markerSize: 10 },
+                { x: 0.144227, y:  0.475953, label: "02/05/2021", color: "#4e73df", markerSize: 10 }
 			]
 		}
 		]
 	});
     chart.render();
     var chart2 = new CanvasJS.Chart("chart2", {
+        axisY: {
+		maximum: 0.22,
+        minimum: 0.12,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "line",  
 		dataPoints: [      
-			{ y: 0.205, label: "01/08/2021", color: "#4e73df"},
-			{ y: 0.165,  label: "01/15/2021", color: "#4e73df"},
-			{ y: 0.178,  label: "01/22/2021", color:"#4e73df" },
-            { y: 0.143,  label: "01/29/2021", color:"#4e73df" },
-            { y: 0.144,  label: "02/05/2021", color:"#4e73df" }
+			{ y: 0.205, label: "01/08/2021", color: "#4e73df", markerSize: 10},
+			{ y: 0.165,  label: "01/15/2021", color: "#4e73df", markerSize: 10},
+			{ y: 0.178,  label: "01/22/2021", color:"#4e73df", markerSize: 10 },
+            { y: 0.143,  label: "01/29/2021", color:"#4e73df", markerSize: 10 },
+            { y: 0.144,  label: "02/05/2021", color:"#4e73df", markerSize: 10 }
 		]
 	}]
 });
     chart2.render();
     var chart3 = new CanvasJS.Chart("chart3", {
+        axisY: {
+		maximum: 0.24,
+        minimum: -0.02,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "column",
 		dataPoints: [      
@@ -47,6 +57,11 @@ var chart = new CanvasJS.Chart("chartContainer", {
 });
     chart3.render();
     var chart4 = new CanvasJS.Chart("chart4", {
+        axisY: {
+		maximum: 0.24,
+        minimum: -0.02,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "column",  
 		dataPoints: [      
@@ -60,6 +75,11 @@ var chart = new CanvasJS.Chart("chartContainer", {
 });
     chart4.render();
     var chart5 = new CanvasJS.Chart("chart5", {
+        axisY: {
+		maximum: 0.24,
+        minimum: -0.02,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "column",  
 		dataPoints: [      
@@ -73,36 +93,51 @@ var chart = new CanvasJS.Chart("chartContainer", {
 });
     chart5.render();
     var chartt = new CanvasJS.Chart("chartt", {
+        axisY: {
+		maximum: 0.24,
+        minimum: -0.02,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "column",  
 		dataPoints: [      
 			{ y: 0.017 , label: "01/08/2021", color: "#4e73df"},
 			{ y: 0.025,  label: "01/15/2021", color: "#4e73df"},
-			{ y: 0.006,  label: "01/22/2021", color:"#4e73df" },
-			{ y: 0.008,  label: "01/29/2021", color:"#4e73df" },
+			{ y: -0.006,  label: "01/22/2021", color:"#4e73df" },
+			{ y: -0.008,  label: "01/29/2021", color:"#4e73df" },
 			{ y: 0.122,  label: "02/05/2021", color:"#4e73df" }
 		]
 	}]
 });
     chartt.render();
     var team = new CanvasJS.Chart("team", {
+        axisY: {
+		maximum: 0.22,
+        minimum: -0.08,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "column",  
 		dataPoints: [      
-			{ y: 0.1304 , label: "01/08/2021", color: "#4e73df"},
-			{ y: -0.0059,  label: "01/15/2021", color: "#4e73df"},
+			{ y: 0.1304, label: "01/08/2021", color: "#4e73df"},
+			{ y: 0.0059,  label: "01/15/2021", color: "#4e73df"},
 			{ y: 0.0521,  label: "01/22/2021", color:"#4e73df" },
 			{ y: 0.1855,  label: "01/29/2021", color:"#4e73df" },
-			{ y: -0.0083,  label: "02/05/2021", color:"#4e73df" }
+			{ y: 0.0083,  label: "02/05/2021", color:"#4e73df" }
 		]
 	}]
 });
     team.render();
     var team2 = new CanvasJS.Chart("team2", {
+        axisY: {
+		maximum: 0.22,
+        minimum: -0.08,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "column",  
 		dataPoints: [      
-			{ y: -0.0375 , label: "01/08/2021", color: "#4e73df"},
+			{ y: -0.0375, label: "01/08/2021", color: "#4e73df"},
 			{ y: 0.1017,  label: "01/15/2021", color: "#4e73df"},
 			{ y: 0.1017,  label: "01/22/2021", color:"#4e73df" },
 			{ y: 0.0059,  label: "01/29/2021", color:"#4e73df" },
@@ -112,6 +147,11 @@ var chart = new CanvasJS.Chart("chartContainer", {
 });
     team2.render();
     var team3 = new CanvasJS.Chart("team3", {
+        axisY: {
+		maximum: 0.22,
+        minimum: -0.08,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "column",  
 		dataPoints: [      
@@ -125,6 +165,11 @@ var chart = new CanvasJS.Chart("chartContainer", {
 });
     team3.render();
     var team4 = new CanvasJS.Chart("team4", {
+        axisY: {
+		maximum: 0.22,
+        minimum: -0.08,
+        interval: 0.02
+	   },
 	data: [{        
 		type: "column",  
 		dataPoints: [      
