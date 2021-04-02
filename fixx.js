a53b71210a7f25fe38cad5433f47d5d0b45a397e
Jython-master <43645058+Jython-master@users.noreply.github.com>
2021-04-02 10:36:26 -0700

    Updated charts

---
+++ fixx.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+var chart = new CanvasJS.Chart("chartContainer", { 
+        axisY: {
+		title: "Fact<--Subjectivity-->Opinion"
+	   },
+        axisX: {
+		title: "Negative<--Sentiment (Polarity)-->Positive"
+	   },
+		data: [
+		{
+			type: "scatter",
+			dataPoints: [
+				{ x: 0.204898, y: 0.461359, label: "01/08/2021", color: "#4e73df" },
+				{ x: 0.165018, y:  0.441554, label: "01/15/2021", color: "#4e73df" },
+				{x: 0.178367, y: 0.460202, label: "01/22/2021", color: "#4e73df" },
+				{ x: 0.143397, y:  0.489541, label: "01/29/2021", color: "#4e73df" },
+                { x: 0.144227, y:  0.475953, label: "02/05/2021", color: "#4e73df" }
+			]
+		}
+		]
+	});
+    chart.render();
+    var chart2 = new CanvasJS.Chart("chart2", {
+	data: [{        
+		type: "line",  
+		dataPoints: [      
+			{ y: 0.205, label: "01/08/2021", color: "#4e73df"},
+			{ y: 0.165,  label: "01/15/2021", color: "#4e73df"},
+			{ y: 0.178,  label: "01/22/2021", color:"#4e73df" },
+            { y: 0.143,  label: "01/29/2021", color:"#4e73df" },
+            { y: 0.144,  label: "02/05/2021", color:"#4e73df" }
+		]
+	}]
+});
+    chart2.render();
+    var chart3 = new CanvasJS.Chart("chart3", {
+	data: [{        
+		type: "column",
+		dataPoints: [      
+			{ y: 0.1186, label: "01/08/2021", color: "#4e73df"},
+			{ y: 0.1086,  label: "01/15/2021", color: "#4e73df"},
+			{ y: 0.2344,  label: "01/22/2021", color:"#4e73df" },
+			{ y: 0.1810,  label: "01/29/2021", color:"#4e73df" },
+			{ y: 0.1365,  label: "02/05/2021", color:"#4e73df" }
+		]
+	}]
+});
+    chart3.render();
+    var chart4 = new CanvasJS.Chart("chart4", {
+	data: [{        
+		type: "column",  
+		dataPoints: [      
+			{ y: 0.0625, label: "01/08/2021", color: "#4e73df"},
+			{ y: 0.1364,  label: "01/15/2021", color: "#4e73df"},
+			{ y: 0.0537,  label: "01/22/2021", color:"#4e73df" },
+			{ y: 0.1247,  label: "01/29/2021", color:"#4e73df" },
+			{ y: 0.0558,  label: "02/05/2021", color:"#4e73df" }
+            ]
+	}]
+});
+    chart4.render();
+    var chart5 = new CanvasJS.Chart("chart5", {
+	data: [{        
+		type: "column",  
+		dataPoints: [      
+			{ y: 0.1341 , label: "01/08/2021", color: "#4e73df"},
+			{ y: 0.1012,  label: "01/15/2021", color: "#4e73df"},
+			{ y: 0.1042,  label: "01/22/2021", color:"#4e73df" },
+			{ y: 0.1557,  label: "01/29/2021", color:"#4e73df" },
+			{ y: 0.0777,  label: "02/05/2021", color:"#4e73df" }
+            ]
+	}]
+});
+    chart5.render();
+    var chartt = new CanvasJS.Chart("chartt", {
+	data: [{        
+		type: "column",  
+		dataPoints: [      
+			{ y: 0.017 , label: "01/08/2021", color: "#4e73df"},
+			{ y: 0.025,  label: "01/15/2021", color: "#4e73df"},
+			{ y: 0.006,  label: "01/22/2021", color:"#4e73df" },
+			{ y: 0.008,  label: "01/29/2021", color:"#4e73df" },
+			{ y: 0.122,  label: "02/05/2021", color:"#4e73df" }
+		]
+	}]
+});
+    chartt.render();
+});
